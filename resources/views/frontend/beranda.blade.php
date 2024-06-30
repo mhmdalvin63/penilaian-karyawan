4bcbdf193e9f1e3fe3d5b78a051a39d66bbc3dc8
mhmdalvin63 <alvinbomber87@gmail.com>
2024-06-30 23:29:49 +0700

    [ ADDING ] Set up & Pages Beranda

---
+++ resources/views/frontend/beranda.blade.php
@@ -0,0 +1,25 @@
+@extends('feMain')
+@section('titleFe', 'beranda')
+<link rel="stylesheet" href="{{asset('../frontEnd/css/beranda.css')}}"> 
+<link rel="stylesheet" href="{{asset('../frontEnd/css/global.css')}}"> 
+    
+@section('contentFe')
+    
+    <div id="beranda" class="bg-blue">
+        <div class="container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-6 text-white">
+                    <h1 class="fw-bold">YUK LIHAT NILAI KAMU !</h1>
+                    <div class="d-flex align-items-center gap-3">
+                        <a class="btn-beranda btn-login bg-green" href="#" role="button">Login</a>
+                        <a class="btn-beranda btn-register bg-yellow" href="#" role="button">Register</a>
+                    </div>
+                </div>
+                <div class="col-4 d-flex justify-content-start">
+                    <img class="kalender" src="{{asset('../frontEnd/images/kalender.png')}}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () { return view('frontend.beranda'); });
+Route::get('/departement', function () { return view('frontend.departement'); });
+Route::get('/login', function () { return view('frontend.login'); });
+Route::get('/register', function () { return view('frontend.register'); });
+Route::get('/', function () { return view('frontend.beranda'); });
+Route::get('/', function () { return view('frontend.beranda'); });
